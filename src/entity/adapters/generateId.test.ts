70cc4016ec9f3a8a6c331a8589268e7d306057d6
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-03-30 17:25:31 +0200

    :sparkles: feat: make adapter for id generator

---
+++ src/entity/adapters/generateId.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect, expectTypeOf } from "vitest"
+import { generateId } from "./generateId"
+
+describe("generateId", () => {
+  it("Given generateId function, When called, Then return string", () => {
+    const result = generateId()
+
+    expect(result).not.toBeUndefined()
+    expectTypeOf(result).toEqualTypeOf<string>()
+  })
+})
