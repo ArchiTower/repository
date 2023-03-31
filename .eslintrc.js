07f23b4460dee659252307270a7f4189898e8917
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-03-31 13:23:55 +0200

    :rotating_light: fix: ESLint & Prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: ["architower"],
+
+  overrides: [
+    {
+      files: ["*.eslintrc.js", "commitlint.config.js", "release.config.js"],
+      extends: ["architower/node"],
+      rules: {
+        "n/no-unpublished-require": "off",
+      },
+    },
+  ],
 }
