0101cd87bb1898ca2c68b012318214dbcafaf4fe
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-04-19 21:38:31 +0200

    :label: feat: add ForeignType, Relations types

--- src/entity/interface.ts
+++ src/entity/interface.ts
@@ -36,16 +36,13 @@ export type EntityInternal<
   _syncMap: SyncMap
 }
 
-export interface EntityFactory<
-  TSchema extends Record<string, any>,
-  TDefinitions
-> {
-  createEntity<TInput extends Partial<TSchema>>(
+export type EntityFactory<TSchema extends Record<string, any>, TDefinitions> = {
+  createEntity: <TInput extends Partial<TSchema> = Partial<TSchema>>(
     data: TInput
-  ): Entity<TSchema, TInput, TDefinitions>
-  recoverEntity<TInput extends Partial<TSchema>>(
+  ) => Entity<TSchema, TInput, TDefinitions>
+  recoverEntity: <TInput extends Partial<TSchema>>(
     data: TInput & { id: string }
-  ): Entity<TSchema, TInput, TDefinitions>
+  ) => Entity<TSchema, TInput, TDefinitions>
 }
 
 export interface Relationship<
