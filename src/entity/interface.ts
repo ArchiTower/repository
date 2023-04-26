3f826d71c36b06c2b60a00be4bc2cb3749966bca
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-04-26 21:51:22 +0200

    :recycle: update EntityFactory

--- src/entity/interface.ts
+++ src/entity/interface.ts
@@ -36,13 +36,16 @@ export type EntityInternal<
   _syncMap: SyncMap
 }
 
-export type EntityFactory<TSchema extends Record<string, any>, TDefinitions> = {
-  createEntity: <TInput extends Partial<TSchema> = Partial<TSchema>>(
+export interface EntityFactory<
+  TSchema extends Record<string, any>,
+  TDefinitions
+> {
+  createEntity<TInput extends Partial<TSchema>>(
     data: TInput
-  ) => Entity<TSchema, TInput, TDefinitions>
-  recoverEntity: <TInput extends Partial<TSchema>>(
+  ): Entity<TSchema, TInput, TDefinitions>
+  recoverEntity<TInput extends Partial<TSchema>>(
     data: TInput & { id: string }
-  ) => Entity<TSchema, TInput, TDefinitions>
+  ): Entity<TSchema, TInput, TDefinitions>
 }
 
 export interface Relationship<
