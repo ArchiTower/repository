0dae086af018a0b5b7afb00e6af6a1ba2bfe683c
Mateusz Gostański <mateusz.gostanski@gmail.com>
2023-05-15 15:44:24 +0300

    :sparkles: feat: entity factories

--- src/entity2/factory.test.ts
+++ src/entity2/factory.test.ts
@@ -3,12 +3,13 @@ import {
   describe,
   it,
   expect,
-  expectTypeOf,
   TestRawEntityData,
   beforeEach,
+  expectTypeOf,
 } from "vitest"
 import { entityModelFactory } from "./factory"
 import { makeRepositoryKey } from "src/repositoryKey"
+import { makeSyncKey } from "./sync"
 
 describe("Entity: Factories", () => {
   beforeEach((context) => {
@@ -19,9 +20,13 @@ describe("Entity: Factories", () => {
       },
       "authors"
     >("authors")
+    context.serializedEntity = JSON.stringify({
+      id: "super-unique-id",
+      foo: "bar",
+    })
   })
 
-  describe("Entity: Model Factory", () => {
+  describe("Model Factory", () => {
     it("Given schema, When model factory called, Then return context factory", () => {
       const result = entityModelFactory<TestRawEntityData>()
 
@@ -29,7 +34,7 @@ describe("Entity: Factories", () => {
     })
   })
 
-  describe("Entity: Context Factory", () => {
+  describe("Context Factory", () => {
     it("Given schema, relations, sync keys, When context factory called, Then return entity factories", ({
       syncKeys,
       authorsRepositoryKey,
@@ -88,20 +93,253 @@ describe("Entity: Factories", () => {
     })
   })
 
-  describe("Entity: Entity Factory", () => {
-    it.todo(
-      "Given schema, When entity factory called, Then return entity",
-      () => {}
-    )
+  describe("Entity Factory", () => {
+    it("Given schema, When entity factory called, Then return entity with given data", () => {
+      const entityFactory = entityModelFactory<TestRawEntityData>()()
 
-    it.todo(
-      "Given schema, relations, When entity factory called, Then return entity with relations",
-      () => {}
-    )
+      const data = {
+        foo: "bar",
+      }
 
-    it.todo(
-      "Given schema, sync keys, When entity factory called, Then return entity with sync keys",
-      () => {}
+      const entity = entityFactory.createEntity(data)
+
+      expect(entity).toEqual(
+        expect.objectContaining({
+          foo: "bar",
+        })
+      )
+    })
+
+    it("Given schema, When entity factory called, Then return entity with unique identifier", () => {
+      const entityFactory = entityModelFactory<TestRawEntityData>()()
+
+      const data = {
+        foo: "bar",
+      }
+
+      const entity = entityFactory.createEntity(data)
+
+      expect(entity.id).toBeDefined()
+    })
+
+    it("Given schema, relations, When entity factory called, Then return entity with relations", ({
+      authorsRepositoryKey,
+    }) => {
+      const entityFactory = entityModelFactory<TestRawEntityData>()([
+        {
+          id: "author",
+          type: "belongs-to",
+          foreignRepository: authorsRepositoryKey,
+          foreignKey: "id",
+          localKey: "bar",
+        },
+      ])
+      const data = {
+        foo: "bar",
+      }
+
+      const entity = entityFactory.createEntity(data)
+
+      expect(entity.author).toBeDefined()
+      expect(entity.author).toBeTypeOf("function")
+    })
+
+    it("Given schema, sync keys, When entity factory called, Then return entity with sync keys", () => {
+      const firstSyncKey = makeSyncKey("foo")
+      const entityFactory = entityModelFactory<TestRawEntityData>()(
+        [],
+        [firstSyncKey]
+      )
+      const data = {
+        foo: "bar",
+      }
+
+      const entity = entityFactory.createEntity(data)
+
+      expect(entity.isSynced(firstSyncKey)).toBe(false)
+    })
+  })
+
+  describe("Recover Entity", () => {
+    it("Given serialized entity, When recover entity, Then return entity with given data, And the same ID", ({
+      serializedEntity,
+    }) => {
+      const entityFactory = entityModelFactory<TestRawEntityData>()()
+
+      const recoveredEntity = entityFactory.recoverEntity(serializedEntity)
+
+      expect(recoveredEntity).toEqual(
+        expect.objectContaining({
+          id: "super-unique-id",
+          foo: "bar",
+        })
+      )
+    })
+
+    it("Given serialized entity, When recover entity, Then return entity with relations", ({
+      authorsRepositoryKey,
+      serializedEntity,
+    }) => {
+      const entityFactory = entityModelFactory<TestRawEntityData>()([
+        {
+          id: "author",
+          type: "belongs-to",
+          foreignRepository: authorsRepositoryKey,
+          foreignKey: "id",
+          localKey: "bar",
+        },
+      ])
+
+      const recoveredEntity = entityFactory.recoverEntity(serializedEntity)
+
+      expect(recoveredEntity.author).toBeDefined()
+      expect(recoveredEntity.author).toBeTypeOf("function")
+    })
+
+    it("Given serialized entity, When recover entity, Then return entity with sync keys all set to un-up-to-date", ({
+      serializedEntity,
+    }) => {
+      const firstSyncKey = makeSyncKey("foo")
+      const entityFactory = entityModelFactory<TestRawEntityData>()(
+        [],
+        [firstSyncKey]
+      )
+
+      const recoveredEntity = entityFactory.recoverEntity(serializedEntity)
+
+      expect(recoveredEntity.isSynced(firstSyncKey)).toBe(false)
+    })
+  })
+})
+
+describe("Entity", () => {
+  it("Given entity, When update, Then update data, And set sync status to un-up-to-date, And keep the same ID, And create new object, And keeps relations untouched", async ({
+    authorsRepositoryKey,
+  }) => {
+    const firstSyncKey = makeSyncKey("foo")
+    const secondSyncKey = makeSyncKey("bar")
+    const { createEntity } = entityModelFactory<TestRawEntityData>()(
+      [
+        {
+          id: "author",
+          type: "belongs-to",
+          foreignRepository: authorsRepositoryKey,
+          foreignKey: "id",
+          localKey: "bar",
+        },
+      ],
+      [firstSyncKey, secondSyncKey]
     )
+    const entity = createEntity({
+      foo: "bar",
+    })
+    const testPromise = Promise.resolve()
+    entity.setSynced(firstSyncKey, testPromise)
+    await testPromise
+    expect(entity.isSynced(firstSyncKey)).toBe(true)
+
+    const updatedEntity = entity.update({
+      foo: "baz",
+      some: false,
+    })
+
+    expect(updatedEntity).not.toBe(entity)
+    expect(updatedEntity.id).toBe(entity.id)
+    expect(updatedEntity.foo).toBe("baz")
+    expect(updatedEntity.some).toBe(false)
+
+    expect(updatedEntity.isSynced(firstSyncKey)).toBe(false)
+    expect(updatedEntity.isSynced(secondSyncKey)).toBe(false)
+
+    expect(updatedEntity.author).toBeDefined()
+    expectTypeOf(updatedEntity.author).toEqualTypeOf<
+      () => { id: string; name: string }
+    >()
+    expect(updatedEntity.author).toBeTypeOf("function")
+  })
+
+  it("Given entity, When update to add data properties, Then updated entity have both data properties", () => {
+    const { createEntity } = entityModelFactory<TestRawEntityData>()()
+    const entity = createEntity({
+      foo: "bar",
+    })
+
+    const updatedEntity = entity.update({
+      some: false,
+    })
+
+    expect(updatedEntity).not.toBe(entity)
+    expect(updatedEntity.foo).toBe("bar")
+    expect(updatedEntity.some).toBe(false)
+  })
+
+  it("Given entity, When update with id, Then ID is not updated", () => {
+    const { createEntity } = entityModelFactory<TestRawEntityData>()()
+    const entity = createEntity({
+      foo: "bar",
+    })
+
+    const updatedEntity = entity.update({
+      // @ts-expect-error - intentionally testing invalid update
+      id: "some-id",
+    })
+
+    expect(updatedEntity).not.toBe(entity)
+    expect(updatedEntity.id).toBe(entity.id)
+    expect(updatedEntity.id).not.toBe("some-id")
+  })
+
+  it("Given entity, When serialize, Then return serialized entity", () => {
+    const { createEntity } = entityModelFactory<TestRawEntityData>()()
+    const data = {
+      foo: "bar",
+    }
+    const entity = createEntity(data)
+
+    const serializedEntity = entity.toJson()
+
+    expect(JSON.parse(serializedEntity)).toEqual({
+      ...data,
+      id: entity.id,
+    })
+  })
+
+  it("Given entity, When dumping to regular object, Then return regular object with data", () => {
+    const { createEntity } = entityModelFactory<TestRawEntityData>()()
+    const data = {
+      foo: "bar",
+    }
+    const entity = createEntity(data)
+
+    const dumpedObject = entity.toObject()
+
+    expect(dumpedObject).toEqual({
+      ...data,
+      id: entity.id,
+    })
+  })
+
+  it("Given entity, When setSynced, Then update sync status only", async () => {
+    const firstSyncKey = makeSyncKey("foo")
+    const secondSyncKey = makeSyncKey("bar")
+    const entityFactory = entityModelFactory<TestRawEntityData>()(
+      [],
+      [firstSyncKey, secondSyncKey]
+    )
+    const data = {
+      foo: "bar",
+    }
+    const entity = entityFactory.createEntity(data)
+    expect(entity.isSynced(firstSyncKey)).toBe(false)
+    expect(entity.isSynced(secondSyncKey)).toBe(false)
+    const id = entity.id
+
+    const fakePromise = Promise.resolve()
+    entity.setSynced(firstSyncKey, fakePromise)
+    await fakePromise
+
+    expect(entity.isSynced(firstSyncKey)).toBe(true)
+    expect(entity.isSynced(secondSyncKey)).toBe(false)
+    expect(entity.id).toBe(id)
   })
 })
--- src/entity2/factory.ts
+++ src/entity2/factory.ts
@@ -5,10 +5,15 @@ import {
   UserDefinedSchema,
   SyncKey,
   ProxyTarget,
+  AllowedEntityInput,
+  EntityData,
+  Entity,
+  EntityPrototype,
 } from "./interface"
 import { relationAccessorFactory } from "./relationsAccessor"
 import { createInternalEntity } from "./proto"
 import { proxyHandlerFactory } from "./proxyHandlerFactory"
+import { generateId } from "./generateId"
 
 export function entityModelFactory<TInputSchema extends UserDefinedSchema>() {
   type ModelSchema = EntitySchema<TInputSchema>
@@ -24,23 +29,63 @@ export function entityModelFactory<TInputSchema extends UserDefinedSchema>() {
 
     const internalEntityClass = createInternalEntity<ModelSchema>(syncKeys)
 
-    const proxyHandler = proxyHandlerFactory<ProxyTarget>()
+    const proxyHandler = proxyHandlerFactory<ProxyTarget>(updateEntity)
 
-    // TODO: Add proper input values & types
-    function createEntity() {
-      const internalEntity = new internalEntityClass()
+    function updateEntity<TUpdatedData extends AllowedEntityInput<ModelSchema>>(
+      this: { proto: EntityPrototype<ModelSchema, EntityData<ModelSchema>> },
+      updatedData: TUpdatedData
+    ): any {
+      const updatedInternalEntity = this.proto.update(updatedData)
 
-      const proxyTarget = {
-        proto: internalEntity,
-        relationAccessor: relationAccessor,
-      }
+      const proxyTarget = createProxyTarget(updatedInternalEntity)
 
       return new Proxy(proxyTarget, proxyHandler)
     }
 
+    function createProxyTarget<TInternalEntity>(
+      internalEntity: TInternalEntity
+    ) {
+      return {
+        proto: internalEntity,
+        relationAccessor: relationAccessor,
+      }
+    }
+
+    function createEntity<TInputData extends AllowedEntityInput<ModelSchema>>(
+      inputData: TInputData
+    ) {
+      const id = generateId()
+      const data = { ...inputData, id } satisfies EntityData<TInputData>
+
+      const internalEntity = new internalEntityClass(data)
+
+      const proxyTarget = createProxyTarget(internalEntity)
+
+      return new Proxy(proxyTarget, proxyHandler) as unknown as Entity<
+        ModelSchema,
+        typeof data,
+        typeof definitions
+      >
+    }
+
+    function recoverEntity(serializedData: string) {
+      const data = JSON.parse(serializedData) as EntityData<
+        AllowedEntityInput<ModelSchema>
+      >
+      const internalEntity = new internalEntityClass(data)
+
+      const proxyTarget = createProxyTarget(internalEntity)
+
+      return new Proxy(proxyTarget, proxyHandler) as unknown as Entity<
+        ModelSchema,
+        typeof data,
+        typeof definitions
+      >
+    }
+
     return {
       createEntity,
-      recoverEntity: () => {},
+      recoverEntity,
     }
   }
 }
